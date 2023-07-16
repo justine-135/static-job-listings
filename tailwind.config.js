5476e8bbfb4c00bb390763201993303ac59656d6
Justine <ujusine@yahoo.com>
2023-07-16 23:32:36 +0800

    added custom classes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        default: "League Spartan, sans-serif",
+      },
+    },
+    colors: {
+      lgcBg: "hsl(180, 52%, 96%)",
+      lgcFilterTabs: "hsl(180, 31%, 95%)",
+      dg: "hsl(180, 8%, 52%)",
+      vdg: "hsl(180, 14%, 20%)",
+      borderRight: "#5DA4A6",
+      borderTopTags: "#BEC0BF",
+    },
+    fontSize: {
+      15: "0.938rem",
+    },
+  },
+  plugins: [],
+};
