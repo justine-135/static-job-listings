f88edd48fd4e5c780d1637b4e4cdcf3774f012d8
Justine <ujusine@yahoo.com>
2023-07-17 16:11:37 +0800

    add helmet package to set bg color to body tag

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <Helmet>
+        <style>{"body { background-color: hsl(180, 52%, 96%); }"}</style>
+      </Helmet>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>
 );
